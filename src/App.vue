89f940f474d95ad21a84065f2b1e7f99e84ae819
harry <dengchao2056@126.com>
2018-02-02 10:05:15 +0800

    remove sensitive

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,14 @@
       multiple
       :name="upload.name"
       :accept="upload.accept"
-      @change="onBatchUpload"
     />
-    <button id="submit-upload" ref="submitUpload">上传</button>
+    <button id="submit-upload" ref="submitUpload" style="display: none;">上传</button>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-import OSSUpload from './upload'
+import OSSUpload from 'oss-upload-direct'
 
 export default {
   name: 'app',
@@ -30,75 +29,53 @@ export default {
     }
   },
   mounted() {
-    const instance = new OSSUpload({
+    this.handleOSSUpload({
       accessid: '6MKOqxGiGU4AUk44',
       accesskey: 'ufu7nS8kS59awNihtjSonMETLI0KLy'
     })
-    const that = this
-    instance.init_plupload({
-      browse_button: this.$refs.filterUpload,
-      // 绑定点击事件
-      bindUploadEvent: new Promise((resolve) => {
-        that.$refs.submitUpload.onclick = (event) => {
-          resolve(event)
-          return false
-        }
-      }),
-      filesAdded(up, files) {
-        console.log('filesAdded: ')
-        console.log(up, files)
-      },
-      beforeUpload(up, files) {
-        console.log('beforeUpload: ')
-        console.log(up, files)
-      },
-      uploadProgress(up, file) {
-        console.log('uploadProgress: ')
-        console.log(up, file)
-      },
-      fileUploaded(up, file, info) {
-        console.log('fileUploaded: ')
-        console.log(up, file, info)
-      },
-      error(up, err) {
-        console.log('error: ')
-        console.log(up, err)
-      }
-    })
   },
   methods: {
-    // 批量上传
-    onBatchUpload(event) {
-      const { files } = event.target || {}
-      console.log(files)
-      return undefined
-      this.handleGetToken()
-        .then((res) => {
-          console.info(res)
-        })
-        .catch((error) => {
-          console.error(error)
-        })
-    },
-    // 获取OSS token
-    handleGetToken() {
-      return new Promise((resolve, reject) => {
-        axios
-          .get('http://test.tjapi.gxtr9.com:5396/tj/oss/getOssToken?appid=sys_127')
-          .then((response) => {
-            const { code, data, message } = response.data || {}
-            if (code === 0) {
-              resolve(data)
-            } else {
-              reject(message)
-            }
-          })
-          .catch(reject)
+    // OSS直传初始化
+    handleOSSUpload(option = {}) {
+      const {
+        accessid,
+        accesskey
+      } = option
+      const instance = new OSSUpload({
+        accessid,
+        accesskey
       })
-    },
-    // 上传文件到OSS
-    handleUploadToOSS() {
-      
+      const that = this
+      instance.init_plupload({
+        browse_button: this.$refs.filterUpload,
+        // 绑定点击事件
+        bindUploadEvent: new Promise((resolve) => {
+          that.$refs.submitUpload.onclick = (event) => {
+            resolve(event)
+          }
+        }),
+        filesAdded(up, files) {
+          console.log('filesAdded: ')
+          console.log(up, files)
+          that.$refs.submitUpload.click()
+        },
+        beforeUpload(up, files) {
+          console.log('beforeUpload: ')
+          console.log(up, files)
+        },
+        uploadProgress(up, file) {
+          console.log('uploadProgress: ')
+          console.log(up, file)
+        },
+        fileUploaded(up, file, info) {
+          console.log('fileUploaded: ')
+          console.log(up, file, info)
+        },
+        error(up, err) {
+          console.log('error: ')
+          console.log(up, err)
+        }
+      })
     }
   }
 }
